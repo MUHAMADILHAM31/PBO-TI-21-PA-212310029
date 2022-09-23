ee8f69c232fde046cdf69f06a23fdce93e4e16e7
M Ilham <milhamfachririzal31@gmail.com>
2022-09-24 00:07:45 +0700

    first commit

---
+++ Praktikum_1_runtunan/src/Latihan08.java
@@ -0,0 +1,18 @@
+public class Latihan08 {
+
+	public static void main(String[] args) {
+		int npm = 212310029;
+		int semester = 3;
+		double ipk = 3.8;
+		String nama = "Muhamad ilham";
+		String kelas = "TI-21-PA";
+		
+		System.out.println("NPM : " + npm);
+		System.out.println("Nama Lengkap : " + nama);
+		System.out.println("Nama Kelas : " + kelas);
+		System.out.println("Semester : " + semester);
+		System.out.println("IPK : " + ipk);
+
+	}
+
+}
